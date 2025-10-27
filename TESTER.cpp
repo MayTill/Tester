9701c0180ffb9125d8ce32ad210da8d652c59b68
Robert "MayTill <rozkladjazdy2424robert2424@gmail.com>
2025-10-27 19:06:37 +0100

    Update TESTER.cpp
    Changing anything Related to AGPL to GPL and using SPDX comments

--- TESTER.cpp
+++ TESTER.cpp
@@ -5,23 +5,17 @@
 #include <atomic>
 #include <iomanip>
 /*
-Tester,Test you drive speed
-Copyright (C) 2025  "MayTill"
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-*/
+/*
+ * Tester, Test your drive speed
+ * Copyright (C) 2025 "MayTill"
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * This file is part of the project "Tester".
+ * See LICENSE file for details.
+ * If you don't get full file get it at 
+ * https://github.com/MayTill/Tester/blob/main/TESTER.cpp
+ */
 using namespace std;
 using namespace std::chrono;
 
@@ -189,3 +183,4 @@ int main()
     AlignedFree(buffer);
     return 0;
 }
+
